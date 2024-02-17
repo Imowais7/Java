dfc24ade5523f4c624c5b8d6c3cc3f3f2e9aa55a
Owais Shakil Ansari <owaisansari9860@gmail.com>
2024-02-17 13:52:36 +0530

    first commit

---
+++ Program1.java
@@ -0,0 +1,13 @@
+// This m first java program
+
+class Program1
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Sheela");
+			System.out.println(20);
+			System.out.println(5.7);
+			System.out.print(60);
+		}
+
+}
