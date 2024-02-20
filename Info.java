6ea39057a917aae4617cff6b98eb74ac4c251558
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-20 17:12:25 +0530

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,15 @@
+class Info
+{
+
+	public static void main(String[] args)
+	{
+		String bankName="panjaab national bank";
+		long accNo=123456789;
+		String ifscCode="648357hwek";
+		int balance=10;
+		System.out.println(bankName);
+		System.out.println(accNo);
+		System.out.println(ifscCode);
+		System.out.println(balance);
+	}
+}
