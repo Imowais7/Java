7df19fb58f8f59411ff1058f3a9862235d8557ce
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-24 22:57:45 +0530

    Add files via upload

---
+++ NegetiveNo.java
@@ -0,0 +1,10 @@
+class NegetiveNo
+{
+	static public void main(String[] args)
+	{
+		int a=5;
+		int b=-6;
+		String c=b<0 ? "Negetive" : "Posetive";
+		System.out.println(b+" Number is "+c);
+	}
+} 
