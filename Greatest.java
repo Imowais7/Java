8415d1b6cfb8c9f9b11c2228d28b440a2f96f78d
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-28 16:54:51 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,17 @@
+// Write a program to find greatest of two numbers
+
+class Greatest {
+    public static void main(String[] args)
+    {
+        int a=20;
+        int b=40;
+        int c =100;
+        if(a>b && a>c)
+            System.out.println(a+" is freatest number");
+        else if(b>c)
+            System.out.println(b+" is a greatest number");
+        else
+            System.out.println(c+" is the greatest of three ");
+    }
+    
+}
