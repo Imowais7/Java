d1d0c821c320e0b6064c3fc7b60d36351f63fcea
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-03-02 21:23:34 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,19 @@
+public class Odd {
+   
+    public static void main(String[] args)
+    {
+        int i=10;
+        while(i>=0)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(i);
+
+            }
+            i--; 
+        }
+           
+    }
+        
+    
+}
