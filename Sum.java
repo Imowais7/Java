de9f3616e1bb2234d689633e0f4688202baee247
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-03-02 21:25:27 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,16 @@
+// import java.util.Scanner;
+public class Sum {
+
+    public static void main(String[] args) {
+        // Scanner inp = new Scanner(System.in);
+
+        int arr[] = { 3, 2, 4 };
+        // arr = inp.
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+
+}
