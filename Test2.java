053a530a998193c8e46512bfe3e094c0811b70f0
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-03-10 17:37:37 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,13 @@
+public class Test2 {
+    public static void main(String[] args)
+    {
+        add();
+
+    }
+    public static void add(){
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+
+    }
+}
