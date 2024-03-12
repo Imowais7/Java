b14c22c22451f12947f82a643e459c55f0dd9b18
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-03-13 00:31:30 +0530

    Add files via upload

---
+++ Test9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Test9 {
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter 1st no");
+        int a =s.nextInt();
+        System.out.println("enter second no");
+        int b=s.nextInt();
+        int c=a+b;
+        System.out.println(c);
+    }
+}
