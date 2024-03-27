075aba0ee9338e6e09e75f38efe0fe211e12e75d
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-03-27 20:25:33 +0530

    Add files via upload

---
+++ Test11.java
@@ -0,0 +1,14 @@
+public class Test11 {
+    static String name="sheela";
+    public static void main(String[] args)
+    {
+        System.out.println(name);
+        name="laila";
+        m1();
+    }
+    public static void m1()
+    {
+        System.out.println(name);
+    }
+    
+}
