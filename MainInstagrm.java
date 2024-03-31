61cc08b1976a59e8b9da2134588e74033e8f6aa3
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-03-31 20:19:24 +0530

    Add files via upload

---
+++ MainInstagrm.java
@@ -0,0 +1,14 @@
+public class MainInstagrm {
+    public static void main(String[] args)
+
+    {
+        Instagram i1 = new Instagram();
+        i1.Initailize("Sheela_lolipop_lover", "princes", 1234567890);
+        Instagram i2 = new Instagram();
+        i2.Initailize("baburao_mango_lover", "lover", 675725498);
+        i1.profile();
+        System.out.println("------------------------------");
+        i2.profile();
+
+    }
+}
